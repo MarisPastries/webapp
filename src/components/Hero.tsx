336319dcd83570407f5c9937ace225b6e51f754f
Antony Cabeza <antonycabeza@gmail.com>
2023-12-12 19:09:03 -0300

    [Update] Hero responsive

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -42,30 +42,37 @@ const Hero = () => {
   };
 
   return (
-    <section className='relative flex justify-center'>
+    <section className='relative flex justify-center max-w-[1540px] mx-auto'>
       <Image
         src={heroImages[selectedIndex].src}
         alt='hero image'
         width={500}
         height={500}
-        className='w-screen h-[600px] object-cover'
+        className='w-screen h-[600px] object-cover md:hidden'
+      />
+      <Image
+        src={heroImages[selectedIndex].src2}
+        alt='hero image'
+        width={1024}
+        height={576}
+        className='hidden w-screen h-[600px] object-cover md:flex 2xl:h-[800px]'
       />
       <BsChevronLeft
-        className='text-[40px] rounded-full py-2 bg-secondary bg-opacity-50 text-light absolute z-10 top-[250px] left-[16px]'
+        className='text-[40px] rounded-full py-2 bg-secondary bg-opacity-50 text-light absolute z-10 top-[250px] left-[16px] md:top-[350px]'
         onClick={prevImage}
       >
         {'<'}
       </BsChevronLeft>
       <BsChevronRight
-        className='text-[40px] rounded-full py-2 bg-secondary bg-opacity-50 text-light absolute z-10 top-[250px] right-[16px]'
+        className='text-[40px] rounded-full py-2 bg-secondary bg-opacity-50 text-light absolute z-10 top-[250px] right-[16px] md:top-[350px]'
         onClick={nextImage}
       >
         {'>'}
       </BsChevronRight>
-      <p className='text-xl font-bold absolute bottom-[50px] left-[20px] drop-shadow-xl'>
+      <p className='text-xl font-bold absolute bottom-[60px] drop-shadow-xl md:text-2xl bg-pink w-fit px-4 py-1 rounded-xl'>
         {heroImages[selectedIndex].name}
       </p>
-      <div className='flex gap-3 absolute bottom-[20px] text-secondary'>
+      <div className='flex gap-3 absolute bottom-[20px] text-secondary font-bold md:text-xl'>
         {selectedIndex === 0 ? <BsCircleFill /> : <BsCircle />}
         {selectedIndex === 1 ? <BsCircleFill /> : <BsCircle />}
         {selectedIndex === 2 ? <BsCircleFill /> : <BsCircle />}
