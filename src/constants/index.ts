41224e61b308adefabd0c709ee1bf72501a3bdf7
Antony Cabeza <antonycabeza@gmail.com>
2023-12-12 19:17:59 -0300

    [Add] Constantes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,18 +1,17 @@
 const pastelesClasicos = {
   name: 'Pasteles Clásicos',
   src: 'https://res.cloudinary.com/dchchsyil/image/upload/v1701994582/marispastries/o6xtdjzvupjw0ff5qy07.jpg',
-};
-const pastelesClasicos2 = {
-  name: 'Pasteles Clásicos',
-  src: 'https://res.cloudinary.com/dchchsyil/image/upload/v1697990158/marispastries/vv5u4xvisnqkx5727qwp.jpg',
+  src2: 'https://res.cloudinary.com/dchchsyil/image/upload/v1702399894/marispastries/oispm8fnjatb64hidhix.jpg',
 };
 const postresIndividuales = {
   name: 'Postres Individuales',
   src: 'https://res.cloudinary.com/dchchsyil/image/upload/v1698085408/marispastries/tqaqorhai4iuokgai6rk.jpg',
+  src2: 'https://res.cloudinary.com/dchchsyil/image/upload/v1702399894/marispastries/f53tb2d4gnmsjjf6ge87.jpg',
 };
 const pieTartas = {
   name: 'Pies y Tartas',
   src: 'https://res.cloudinary.com/dchchsyil/image/upload/v1701995115/marispastries/m2d7rmva1vfjoywo779u.jpg',
+  src2: 'https://res.cloudinary.com/dchchsyil/image/upload/v1702399894/marispastries/seens4lnwxg4lymfeo0f.jpg',
 };
 
 export const heroImages = [pastelesClasicos, postresIndividuales, pieTartas];
