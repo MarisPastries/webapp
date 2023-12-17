5cd4942c76d05cda832126fc5b58984acac14d87
Antony Cabeza <antonycabeza@gmail.com>
2023-12-16 22:14:05 -0300

    [Update] Copy en paginas de construccion

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,7 @@
 export default function Menu() {
   return (
     <main className='w-full h-screen flex items-center justify-center'>
-      <h1 className='text-red-400 font-extrabold text-5xl'>
-        Mari's Pastries, coming soon... (Menú)
-      </h1>
+      <p>En construcción...</p>
     </main>
   );
 }
